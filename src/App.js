923156bfa10fa2ece4ad2c59a92c59a160dcdf42
longpt271 <longpt271@gmail.com>
2022-10-21 15:40:01 +0700

    2. Tạo Router cho ứng dụng

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Home from './pages/HomePage';
+import Shop from './pages/ShopPage';
+import Detail from './pages/DetailPage';
+import Cart from './pages/CartPage';
+import Checkout from './pages/CheckoutPage';
+import Login from './pages/LoginPage';
+import Register from './pages/RegisterPage';
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/shop" component={Shop} />
+      <Route path="/detail/:id" component={Detail} />
+      <Route path="/cart" component={Cart} />
+      <Route path="/checkout" component={Checkout} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="*" component={Home} />
+    </Switch>
+  );
 }
 
 export default App;
