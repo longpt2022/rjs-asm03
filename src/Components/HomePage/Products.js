aef8a68ea92fa66788ac918183e117ca2fa2dfbe
longpt271 <longpt271@gmail.com>
2022-10-23 20:13:22 +0700

    4c. Danh sách các sản phẩm

---
+++ src/Components/HomePage/Products.js
@@ -0,0 +1,85 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import LoadingSpinner from '../UI/LoadingSpinner/LoadingSpinner';
+import classes from './Products.module.css';
+
+const Products = () => {
+  // State lưu kết quả fetch
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [productsData, setProductsData] = useState([]);
+  // console.log(productsData);
+
+  //  Hàm fetch xử lý api
+  const sendRequest = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        'https://firebasestorage.googleapis.com/v0/b/funix-subtitle.appspot.com/o/Boutique_products.json?alt=media&token=dc67a5ea-e3e0-479e-9eaf-5e01bcd09c74'
+      );
+
+      // Nếu gặp lỗi thả ra lỗi
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
+
+      const data = await response.json();
+
+      // set data trả về với 8 phần tử đầu tiên
+      setProductsData(data.slice(0, 8));
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  // Hàm chuyển đổi thành dạng chuỗi vào bổ sung các dấu chấm ngăn cách giữa các đơn vị
+  const transformPrice = string => {
+    return string.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  };
+
+  return (
+    <section className={`${classes.products} mb-5`}>
+      <div className="mb-4">
+        <span className="text-secondary">MADE THE HARD WAY</span>
+        <h4>TOP TRENDING PRODUCTS</h4>
+      </div>
+      <div className="row">
+        {isLoading && (
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        )}
+        {!isLoading && error && <p>{error}</p>}
+        {!isLoading &&
+          !error &&
+          productsData.map(product => {
+            return (
+              <div
+                key={product._id.$oid}
+                className="col-lg-3 col-md-12 mb-4 mb-lg-3 text-center"
+              >
+                <img
+                  src={product.img1}
+                  alt={product.category}
+                  className="w-100 mb-3"
+                />
+                <p className="fw-bold mb-1">{product.name}</p>
+                <span className="text-secondary">
+                  {transformPrice(product.price)}
+                </span>
+              </div>
+            );
+          })}
+      </div>
+    </section>
+  );
+};
+
+export default Products;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import Banner from '../Components/HomePage/Banner';
 import Categories from '../Components/HomePage/Categories';
+import Products from '../Components/HomePage/Products';
 
 const HomePage = () => {
   return (
     <>
       <Banner />
       <Categories />
+      <Products />
     </>
   );
 };
