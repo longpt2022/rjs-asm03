8f0692e6851b68c5bf3008f1fd50518f135633b7
longpt271 <longpt271@gmail.com>
2022-10-28 16:35:21 +0700

    9b. (UPDATE_CART)

--- src/Components/CartPage/CartList.js
+++ src/Components/CartPage/CartList.js
@@ -1,9 +1,15 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons';
+
+import { cartActions } from 'store/cart';
 
 const CartList = () => {
   // Lấy state redux
   const listCart = useSelector(state => state.cart.listCart);
   const totalQuantity = useSelector(state => state.cart.totalQuantity);
+
+  const dispatch = useDispatch();
 
   // Biến xác định có cart nào k
   const hasCart = listCart.length > 0;
@@ -13,27 +19,75 @@ const CartList = () => {
     return curNumber + item.quantity;
   }, 0);
 
+  // Hàm xử lý giảm 1
+  const clickLeftHandler = productData => {
+    if (productData.quantity > 1) {
+      dispatch(
+        cartActions.UPDATE_CART({
+          // data product truyền từ cha
+          ...productData,
+          // số lượng product nhập vào input
+          quantity: -1,
+        })
+      );
+    }
+  };
+
+  // Hàm xử lý tăng 1
+  const clickRightHandler = productData => {
+    dispatch(
+      cartActions.UPDATE_CART({
+        // data product truyền từ cha
+        ...productData,
+        // số lượng product nhập vào input
+        quantity: 1,
+      })
+    );
+  };
+
   return (
     <section className="pb-5">
       <div className="d-flex justify-content-between bg-light p-5 mb-4">
         <h4 className="p-4">CART</h4>
         <span className="p-4 text-secondary">CART</span>
       </div>
-      <div>
-        <div>
+      <div className="row">
+        <div className="col-lg-9">
           {listCart.map(cart => {
             return (
-              <div key={cart._id.$oid}>
-                <b>{cart.quantity}</b>
-                <span>{cart.name}</span>
-                <p>{cart.price}</p>
+              <div key={cart._id.$oid} className="no-copy-text row">
+                <span className="col-lg-2">{cart.name}</span>
+                <div className="col-lg-2">
+                  <p>{cart.price}</p>
+                  <span>VND</span>
+                </div>
+                <div className="col-lg-2">
+                  <FontAwesomeIcon
+                    icon={faCaretLeft}
+                    onClick={clickLeftHandler.bind(null, cart)}
+                    className="p-2"
+                  />
+
+                  <b>{cart.quantity}</b>
+                  <FontAwesomeIcon
+                    icon={faCaretRight}
+                    onClick={clickRightHandler.bind(null, cart)}
+                    className="p-2 me-2"
+                  />
+                </div>
+                <div className="col-lg-2">
+                  <p>{cart.price * cart.quantity}</p>
+                  <span>VND</span>
+                </div>
               </div>
             );
           })}
         </div>
-        <p>Total products: {numberOfCartItems}</p>
-        <p>{totalQuantity}</p>
-        {hasCart && <button>Order</button>}
+        <div className="col-lg-3">
+          <p>Total products: {numberOfCartItems}</p>
+          <p>{totalQuantity}</p>
+          {hasCart && <button>Order</button>}
+        </div>
       </div>
     </section>
   );
--- src/store/cart.js
+++ src/store/cart.js
@@ -54,12 +54,46 @@ const cartSlice = createSlice({
         })
       );
     },
-    UPDATE_CART(state) {
-      state.listCart--;
+    UPDATE_CART(state, action) {
+      // logic tương tự add
+      // action.payload.quantity = -1 nếu muốn giảm
+      // action.payload.quantity = +1 nếu muốn tăng
+
+      const updatedTotalQuantity =
+        state.totalQuantity + action.payload.price * +action.payload.quantity;
+
+      // Tìm id trùng với id payload
+      const existingCartItemIndex = state.listCart.findIndex(
+        item => item._id.$oid === action.payload._id.$oid
+      );
+      const existingCartItem = state.listCart[existingCartItemIndex];
+
+      // Gộp số lượng sản phẩm nếu trùng nhau
+      let updatedListCart;
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: +existingCartItem.quantity + +action.payload.quantity,
+        };
+        updatedListCart = [...state.listCart];
+        updatedListCart[existingCartItemIndex] = updatedItem;
+      } else {
+        updatedListCart = state.listCart.concat(action.payload);
+      }
+
+      // Giá trị trả về sau khi update
+      state.listCart = updatedListCart;
+      state.totalQuantity = updatedTotalQuantity;
+      // lưu lại vào localStorage
+      localStorage.setItem(
+        'listCart',
+        JSON.stringify({
+          listCart: updatedListCart,
+          totalQuantity: updatedTotalQuantity,
+        })
+      );
     },
-    DELETE_CART(state, action) {
-      state.listCart = state.listCart + action.payload;
-    },
+    DELETE_CART(state, action) {},
   },
 });
 
